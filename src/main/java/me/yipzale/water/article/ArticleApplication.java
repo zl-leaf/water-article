18b552004daf4b3516f25a54d2f058d1772a49e2
叶志良 <leaf@yezhiliangdeMacBook-Air.local>
2018-09-09 20:58:58 +0800

    使用configserver

--- src/main/java/me/yipzale/water/article/ArticleApplication.java
+++ src/main/java/me/yipzale/water/article/ArticleApplication.java
@@ -1,9 +1,16 @@
 package me.yipzale.water.article;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +18,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ArticleApplication {
     public static void main(String[] args) {
         SpringApplication.run(ArticleApplication.class, args);
+    }
+
+    @Bean(destroyMethod = "close", initMethod = "init")
+    @Primary
+    @ConfigurationProperties(prefix="spring.datasource")
+    @RefreshScope
+    public DataSource dataSource() {
+        return new DruidDataSource();
     }
 }
